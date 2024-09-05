e297c9c6de7e732dd38f90c8fc86d08036a61045
YeRimmm-Cho <em514@dgu.ac.kr>
2024-09-06 02:12:49 +0900

    feat: 그룹 상세 조회 페이지 구현

--- frontend/src/pages/PrivateGroupDetail.js
+++ frontend/src/pages/PrivateGroupDetail.js
@@ -1,19 +1,34 @@
-import React from "react";
-import MemoryCard from "../components/MemoryCard"; // MemoryCard 컴포넌트 가져오기
+import React, { useState } from "react";
+import MemoryCard from "../components/MemoryCard";
 import styles from "./PrivateGroupDetail.module.css";
-import noPostsImage from "../assets/emptyMemory.svg"; // '게시된 추억이 없습니다'를 위한 이미지
+import noPostsImage from "../assets/emptyMemory.svg";
 
 function PrivateGroupDetail({ group, memories }) {
+  const [visibleMemories, setVisibleMemories] = useState(8); // 초기 상태로 8개의 메모리를 표시
+
+  const handleLoadMore = () => {
+    setVisibleMemories((prev) => prev + 8); // 더보기 버튼 클릭 시 8개씩 추가로 표시
+  };
+
   return (
     <div className={styles.groupDetail}>
       <div className={styles.posts}>
-        {/* 게시글이 있을 때와 없을 때를 구분 */}
         {memories.length > 0 ? (
-          <div className={styles.postList}>
-            {memories.map((post) => (
-              <MemoryCard key={post.id} post={post} isPublic={false} />
-            ))}
-          </div>
+          <>
+            <div className={styles.postList}>
+              {memories.slice(0, visibleMemories).map((post) => (
+                <MemoryCard key={post.id} post={post} isPublic={false} />
+              ))}
+            </div>
+            {visibleMemories < memories.length && (
+              <button
+                className={styles.loadMoreButton}
+                onClick={handleLoadMore}
+              >
+                더보기
+              </button>
+            )}
+          </>
         ) : (
           <div className={styles.noPosts}>
             <img src={noPostsImage} alt="No posts" />
